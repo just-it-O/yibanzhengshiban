2b650081aa4cb7617c29037dadf3787cc1e098f3
just-it-O <961411870@qq.com>
2025-05-05 17:16:08 +0800

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import App from '../App.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: App },
+    { path: '/home', component: App },
+    { path: '/about', component: App },
+    { path: '/form', component: App }
+  ]
+})
+
+export default router
